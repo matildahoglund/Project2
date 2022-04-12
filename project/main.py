d6ae4a9e7ddc92c75b019b2d20407cf7a445b219
Emanuelgottfridsson <77583035+Emanuelgottfridsson@users.noreply.github.com>
2022-04-12 14:43:02 +0200

    Update main.py
    Jag la in koden som han skrev innan
    :D

--- project/main.py
+++ project/main.py
@@ -2,6 +2,24 @@
 import sys
 import __init__
 
+from pybrick.evdevices import Motor, colorsensor
+from pbricks.parameters import port,direction,stop
+from pythbticks.tools Import wait
+from pybticks.robotics import DriveBase
+import __init__
+
+
+lift_motor = Motor(Port.B, positive_direction-Direction.CLOCKWISE, gears = [ 12,36])
+
+lef_motor = Motor(Port.B, positive_direction-Direction.CLOCKWISE, gears = [ 12,20])
+rigt_motor = Motor(Port.B, positive_direction-Direction.CLOCKWISE, gears = [ 12,20])
+
+nonne = DriveBase( left_motor, right_motor, wheel diameter = 47, axle_track = 128)  
+def main():
+    bobbe.straight(100) """ 100 är i millimeter"""
+    return 0
+
+
 def main():
     return 0
 
