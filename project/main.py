d6ea8025ffbe3f78a3ad29083f0a278f1e579e4e
Emanuelgottfridsson <77583035+Emanuelgottfridsson@users.noreply.github.com>
2022-04-19 16:42:34 +0200

    Update main.py
    Har försökt göra lite på andra storyn och gjort funktioner.

--- project/main.py
+++ project/main.py
@@ -6,15 +6,22 @@ from pybrick.evdevices import Motor, colorsensor
 from pbricks.parameters import port,direction,stop
 from pythbticks.tools Import wait
 from pybticks.robotics import DriveBase
-import __init__
 
+def checking_sensor(port):
+    port = TouchSensor(port)
+    return  pressed(port)
 
-lift_motor = Motor(Port.B, positive_direction-Direction.CLOCKWISE, gears = [ 12,36])
+def checking_color(port):
+    return color(port) 
+    lift_motor = Motor(Port.B, positive_direction-Direction.CLOCKWISE, gears = [ 12,36])
+    """ A positive speed value should make the motor move clockwise. """
 
-lef_motor = Motor(Port.B, positive_direction-Direction.CLOCKWISE, gears = [ 12,20])
-rigt_motor = Motor(Port.B, positive_direction-Direction.CLOCKWISE, gears = [ 12,20])
-
-nonne = DriveBase( left_motor, right_motor, wheel diameter = 47, axle_track = 128)  
+def driving(Port):
+    left_motor = Motor(Port.B, positive_direction-Direction.CLOCKWISE, gears = [ 12,20])
+    rigt_motor = Motor(Port.B, positive_direction-Direction.CLOCKWISE, gears = [ 12,20])
+    nonne = DriveBase(left_motor, right_motor, wheel diameter = 47, axle_track = 128)
+    return 
+     
 def main():
     bobbe.straight(100) """ 100 är i millimeter"""
     return 0
@@ -25,3 +32,8 @@ def main():
 
 if __name__ == '__main__':
     sys.exit(main())
+    Touchsensors(port) """titta om sensor är påslagen"""
+    checking_color(port)
+    
+    
+    
