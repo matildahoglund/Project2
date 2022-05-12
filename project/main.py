906d1802a9a5348405295cafec387faf12f728cf
thehallen <103506751+thehallen@users.noreply.github.com>
2022-05-12 10:30:28 +0200

    Update main.py

--- project/main.py
+++ project/main.py
@@ -1,8 +1,5 @@
-# #!/usr/bin/env pybricks-micropython
-# import sys
-# import __init__
+#!/usr/bin/env pybricks-micropython
 
-<<<<<<< Updated upstream
 from pybricks.ev3devices import Motor, ColorSensor, TouchSensor
 from pybricks.parameters import Port, Direction, Stop
 from pybricks.tools import wait
@@ -43,15 +40,6 @@ robot = DriveBase(left_motor, right_motor, wheel_diameter = 55.5, axle_track =10
 light = ColorSensor(Port.S3)
 
 
-# white = [90, 95]
-# Green = 14
-# Blue = 16
-# Blue_new = 8
-# brown = 23
-# lila = 18
-# rosa = 85 87
-
-
 # print(light.reflection())
 
 def lift_forks(lift_motor):
@@ -63,7 +51,7 @@ def lower_forks(lift_motor):
     return
 
  
-#lift when pressed#
+
 def lift_object(lift_motor, lift_forks, lower_forks):
     lower_forks(lift_motor)
     while touch_sensor.pressed() != True:
@@ -72,8 +60,19 @@ def lift_object(lift_motor, lift_forks, lower_forks):
     
     return
 
-Color = {"Brown":[], "Green":[], "Red":[],"Blue":[]}
+Color = {"Brown":[], "Green":[], "Red":[],"Blue":[], "Purple":[], "Yellow":[]}
 
+# Green': [(6, 19, 5), (9, 31, 14), (10, 33, 14), (10, 33, 16), (10, 33, 14), (9, 34, 15), (9, 33, 17), (8, 30, 11), (8, 30, 12), (10, 30, 16)]
+
+Green = 
+
+# 'Brown': [(16, 15, 10), (15, 14, 8), (15, 15, 9), (15, 14, 8), (14, 14, 8), (15, 15, 10), (15, 15, 9), (15, 15, 9)]}
+
+# 'Purple': [(10, 8, 21), (12, 9, 32), (11, 9, 30), (9, 7, 25), (3, 2, 9), (7, 5, 20), (10, 8, 22)]
+
+# 'Blue': [(12, 23, 38), (9, 21, 38), (10, 21, 36), (10, 19, 33), (8, 18, 33), (8, 20, 37), (6, 14, 22), (8, 19, 37), (10, 23, 40), (9, 21, 39), (12, 33, 68), (12, 36, 77), (10, 31, 68), (11, 32, 68), (12, 30, 64)]
+
+# Yellow': [(50, 38, 7), (51, 39, 8), (51, 39, 7), (48, 37, 7), (47, 36, 6), (49, 38, 8), (50, 36, 6), (49, 36, 6), (48, 36, 6), (47, 35, 7), (49, 39, 8)]
 
 def Checking_color(Color):
     loop = True
@@ -87,131 +86,26 @@ def Checking_color(Color):
             print("You have puted in all values")
             return Color
 
-=======
-# from pybricks.ev3devices import Motor, ColorSensor
-# from pybricks.parameters import Port, Direction, Stop
-# from pybricks.tools import wait
-# from pybricks.robotics import DriveBase
-
-
-
-# Color = {"brun":[], "Grön":[], "röd":[],"blå":[]}
-
-    
-
-# left_motor = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE, gears = [ 12,20])
-# right_motor = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE, gears = [ 12,20])
-# bobbe = DriveBase(left_motor, right_motor, wheel_diameter = 47, axle_track = 128)
-# lift_motor = Motor(Port.A, positive_direction=Direction.CLOCKWISE)
-
-# def checking_sensor(port):
-#     port = TouchSensor(port)
-#     return pressed(port)
-
-# def checking_color():
-#     light = ColorSensor(Port.S2)
-#     return color()
-    
-# def main():
-#     bobbe.straight(100)
-#     return 0
-
-# def lift_forks(lift_motor):
-#     lift_motor.run_until_stalled(50, then=Stop.HOLD, duty_limit=80)
-#     return
-    
-# # def sensor_ett(color):
-# #     # Initialize the motors.
-# #     left_motor = Motor(Port.B)
-# #     right_motor = Motor(Port.C)
-# #     # Initialize the color sensor.  
-# #     line_sensor = ColorSensor(Port.S3)
-# #     # Initialize the drive base.
-# #     robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
-# #     # Calculate the light threshold. Choose values based on your measurements.
-# #     BLACK = 9
-# #     WHITE = 85
-# #     threshold = (BLACK + WHITE) / 2
-# #     # Set the drive speed at 100 millimeters per second.
-# #     DRIVE_SPEED = 100
-# #     # Set the gain of the proportional line controller. This means that for every
-# #     # percentage point of light deviating from the threshold, we set the turn
-# #     # rate of the drivebase to 1.2 degrees per second.
-
-# #     # For example, if the light value deviates from the threshold by 10, the robot
-# #     # steers at 10*1.2 = 12 degrees per second.
-# #     PROPORTIONAL_GAIN = 1.2
-# #     # Start following the line endlessly.
-# #     while True:
-# #         # Calculate the deviation from the threshold.
-# #         deviation = line_sensor.reflection() - threshold
-# #         # Calculate the turn rate.
-# #         turn_rate = PROPORTIONAL_GAIN * deviation
-# #         # Set the drive base speed and turn rate.
-# #         robot.drive(DRIVE_SPEED, turn_rate)
-# #         # You can wait for a short time or do other things in this loop.
-# #         wait(30)
-
-
-# """titta om sensor är påslagen"""
-# # if __name__ == '__main__':
-# #     sys.exit(main())
-    
-    
-# # Ok = true
-# # while not ok:
-# #     answer = Input("Vad vill du få roboten att göra..?")
-# #     if answer = 1: 
-# #         sensor(port)
-# #     elif answer = 2:
-        
-# #     elif answer = 3:
-        
-# #     elif answer = 4:
-    
-
-# # ev3 = EV3Brick()
-# # robot = DriveBase(left_motor, right_motor, wheel_diameter = 55.5, axle_track =104)
-# # light = ColorSensor(Port.S2)
-# # touch = TouchSensor(Port.S2)
-# # gyro = GyroSensor(Port.S1)
-
-# # While True:
-# #     while light.reflection() > 15:
-# #         robot.drive(-100,0)
-# #     robot.stop()
-# #     left_motor.brake()
-# #     right_motor.brake()
-# #     while light.reflection() < 50:
-# #         robot.drive(100,0)
-# #     robot.stop()
-# #     left_motor.brake()
-# #     right_motor.brake()
-
-# while True:
-#     print("hej")
-
-#En loop som sätter in rpg värde
-
 def color_function():
     """ Denna funktionen ska hoppa in i vårtconditionet om rpg är inom våra mätta intervall
     och så fort den går ut vårt färgintervall ska den antingen köra framåt i 4 sekunder
     sedan börja läsa av igen eller svänga 45 grader"""
     color = rgb()
-    """ blå"""
-    if 0<color[0] <2,0< color[1]<2, color[2] < 3:
-        our_color = "blå"
+    """ Blue"""
+    if 6<color[0]<12, 14<color[1]<36, 22<color[2]<77:
+        our_color = "Blue"
         sensor_two(color)
-        "röd"
-    elif 0<color[0] <2,0< color[1]<2, color[2] < 3:
-        our_color = "röd"
-        sensor_two(color) 
-    elif 0<color[0] <2,0< color[1]<2, color[2] < 3:
-        our_color = "blå"
-        sensor_two(color)
+        # "Red"
+    # elif <color[0] <2,0< color[1]<2, color[2] < 3:
+    #     our_color = "Red"
+        # sensor_two(color) 
         
     elif 0<color[0] <2,0< color[1]<2, color[2] < 3:
-        our_color ="brun"
+        our_color = "Blue"
+        sensor_two(color)
+ 
+    elif 0<color[0] <2,0< color[1]<2, color[2] < 3:
+        our_color ="Brown"
         sensor_two(color)
     elif 0<color[0] <2,0< color[1]<2, color[2] < 3:
         "our color = black"
@@ -219,42 +113,3 @@ def color_function():
     else: 
         """antingen vänd 45 grader eller kör fram någon sekund"""
     return
-        
-        
-
-Color = {"brun":[], "Grön":[], "röd":[],"blå":[]}
-def Checking_color(Color):
-    loop = True
-    while loop:
-        Colors = input("What color do you want to add?(If you are finish type x): ")     
-        for key in Color.keys():
-            if key == Colors:
-                rpg =  input("Skriv in något: ")
-                Color[key] = rpg
-        if Colors == "X":
-            loop = False
-            print("You have puted in all values") 
-            return Color    
-        
-        
-def sensor_two(color):
-
-    left_motor = Motor(Port.B)
-    right_motor = Motor(Port.C)
-    line_sensor = ColorSensor(Port.S3)
-    robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
-    Colors = Color["Det värdet på rbg()"]
-    WHITE = 85
-    threshold = (Colors + WHITE) / 2
-    DRIVE_SPEED = 100
-    PROPORTIONAL_GAIN = 1.2
-    while True:
-        deviation = color["Det värdet som ger reflektion"] - threshold
-        turn_rate = PROPORTIONAL_GAIN * deviation
-        robot.drive(DRIVE_SPEED, turn_rate)
-        wait(30)
-
-                    
->>>>>>> Stashed changes
-Checking_color(Color)
-print(Color)
