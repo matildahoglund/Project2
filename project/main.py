8b3ebd02b202f1965029b6d7b212d60c8011cf22
thehallen <103506751+thehallen@users.noreply.github.com>
2022-05-12 11:01:38 +0200

    Update main.py

--- project/main.py
+++ project/main.py
@@ -62,17 +62,6 @@ def lift_object(lift_motor, lift_forks, lower_forks):
 
 Color = {"Brown":[], "Green":[], "Red":[],"Blue":[], "Purple":[], "Yellow":[]}
 
-# Green': [(6, 19, 5), (9, 31, 14), (10, 33, 14), (10, 33, 16), (10, 33, 14), (9, 34, 15), (9, 33, 17), (8, 30, 11), (8, 30, 12), (10, 30, 16)]
-
-Green = 
-
-# 'Brown': [(16, 15, 10), (15, 14, 8), (15, 15, 9), (15, 14, 8), (14, 14, 8), (15, 15, 10), (15, 15, 9), (15, 15, 9)]}
-
-# 'Purple': [(10, 8, 21), (12, 9, 32), (11, 9, 30), (9, 7, 25), (3, 2, 9), (7, 5, 20), (10, 8, 22)]
-
-# 'Blue': [(12, 23, 38), (9, 21, 38), (10, 21, 36), (10, 19, 33), (8, 18, 33), (8, 20, 37), (6, 14, 22), (8, 19, 37), (10, 23, 40), (9, 21, 39), (12, 33, 68), (12, 36, 77), (10, 31, 68), (11, 32, 68), (12, 30, 64)]
-
-# Yellow': [(50, 38, 7), (51, 39, 8), (51, 39, 7), (48, 37, 7), (47, 36, 6), (49, 38, 8), (50, 36, 6), (49, 36, 6), (48, 36, 6), (47, 35, 7), (49, 39, 8)]
 
 def Checking_color(Color):
     loop = True
@@ -86,30 +75,34 @@ def Checking_color(Color):
             print("You have puted in all values")
             return Color
 
-def color_function():
+def color_function(sensor):
     """ Denna funktionen ska hoppa in i vårtconditionet om rpg är inom våra mätta intervall
     och så fort den går ut vårt färgintervall ska den antingen köra framåt i 4 sekunder
     sedan börja läsa av igen eller svänga 45 grader"""
-    color = rgb()
-    """ Blue"""
-    if 6<color[0]<12, 14<color[1]<36, 22<color[2]<77:
+    color = light.rgb()
+    print(color)
+    if 6<color[0]<12 and 14<color[1]<36 and 22<color[2]<77:
         our_color = "Blue"
-        sensor_two(color)
-        # "Red"
-    # elif <color[0] <2,0< color[1]<2, color[2] < 3:
-    #     our_color = "Red"
-        # sensor_two(color) 
-        
-    elif 0<color[0] <2,0< color[1]<2, color[2] < 3:
-        our_color = "Blue"
-        sensor_two(color)
+        sensor()
+
+    elif 47<color[0]<51 and 35<color[1]<39 and 6<color[2]<8:
+        our_color = "Yellow"
+        sensor()
  
-    elif 0<color[0] <2,0< color[1]<2, color[2] < 3:
+    elif 14<color[0]<16 and 14<color[1]<15 and 8<color[2] <10:
         our_color ="Brown"
-        sensor_two(color)
-    elif 0<color[0] <2,0< color[1]<2, color[2] < 3:
-        "our color = black"
-        sensor_two(color)
+        sensor()
+
+    elif 3<color[0]<12 and 2<color[1]<9 and 9<color[2]<32:
+        "our color = Purple"
+        sensor()
+    
+    elif 6<color[0]<10 and 19<color[1]<34 and 5<color[2]<17:
+        "our color = Green"
+        sensor()
+
     else: 
         """antingen vänd 45 grader eller kör fram någon sekund"""
     return
+
+color_function(sensor)
